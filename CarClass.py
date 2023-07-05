139202f515e22bfba728b6187f714506ab6c2494
rizza <denostarizza43@gmail.com>
2023-07-05 16:42:36 +1200

    initialize data attributes

--- CarClass.py
+++ CarClass.py
@@ -1,6 +1,9 @@
 class Car:
     def __init__(self, year_model, make):
      # Initialize data attributes
+     self.__year_model = year_model
+     self.__make = make
+     self.__speed = 0
 # Increase the speed by 5
 # Decrease the speed by 5
 # Ensure speed is not negative
