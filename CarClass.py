dd5fb0ec3e26909586ac8f9a451c77b0f7580427
rizza <denostarizza43@gmail.com>
2023-07-05 16:29:31 +1200

    Pseudocode

---
+++ CarClass.py
@@ -0,0 +1,8 @@
+# Initialize data attributes
+# Increase the speed by 5
+# Decrease the speed by 5
+# Ensure speed is not negative
+# Return the current speed
+# Create a Car object
+# Call the accelerate method five times and display current speed
+# Call the brake method five times and display current speed
